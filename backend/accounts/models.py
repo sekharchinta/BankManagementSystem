a1aabd21ae843d4a32b09eb8c9e3681415db1403
Somu003 <sekharchinta160@gmail.com>
2026-07-24 19:59:17 +0530

    feat: implement customer, account, and transaction models

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from customers.models import Customer
 
-# Create your models here.
+
+class Account(models.Model):
+    ACCOUNT_TYPES = (
+        ("Savings", "Savings"),
+        ("Current", "Current"),
+    )
+
+    account_number = models.CharField(max_length=20, unique=True)
+
+    customer = models.ForeignKey(
+        Customer,
+        on_delete=models.CASCADE,
+        related_name="accounts"
+    )
+
+    account_type = models.CharField(
+        max_length=20,
+        choices=ACCOUNT_TYPES
+    )
+
+    balance = models.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+        default=0
+    )
+
+    def __str__(self):
+        return self.account_number
--- backend/transactions/models.py
+++ backend/transactions/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from accounts.models import Account
 
-# Create your models here.
+
+class Transaction(models.Model):
+
+    TRANSACTION_TYPES = (
+        ("Deposit", "Deposit"),
+        ("Withdraw", "Withdraw"),
+        ("Transfer", "Transfer"),
+    )
+
+    account = models.ForeignKey(
+        Account,
+        on_delete=models.CASCADE,
+        related_name="transactions"
+    )
+
+    transaction_type = models.CharField(
+        max_length=20,
+        choices=TRANSACTION_TYPES
+    )
+
+    amount = models.DecimalField(
+        max_digits=12,
+        decimal_places=2
+    )
+
+    balance_after_transaction = models.DecimalField(
+        max_digits=12,
+        decimal_places=2
+    )
+
+    description = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.transaction_type} - {self.amount}"
